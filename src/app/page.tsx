9885a33b2a443ff52c97b83f512f20de42a18862
yosh-25 <yoshi.okada25@gmail.com>
2024-02-14 11:36:05 +0900

    todoList一覧の画面パーツを作成した

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,28 @@ import Link from "next/link";
 
 export default function Home() {
   return (
+    <div className="flex flex-col space-y-10 justify-center items-center mt-10 min-h">
       <div>
-        <h1>Top Page</h1>
-        <Link href='/'>新規登録</Link>
-        <Link href='/'>ログイン</Link>
+        <h1 className="text-3xl font-bold">Top Page</h1>
       </div>
+      <div>
+        <Link href="/">
+          <button
+            type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none"
+          >
+            新規登録
+          </button>
+        </Link>
+        <Link href="/">
+          <button
+            type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none"
+          >
+            ログイン
+          </button>
+        </Link>
+      </div>
+    </div>
   );
 }
---
+++ src/app/todos/page.tsx
@@ -0,0 +1,55 @@
+import Link from 'next/link'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="flex flex-col space-y-10 justify-center items-center mt-10 min-h">
+      <div>
+        <h1 className="text-3xl font-bold">Todo一覧</h1>
+      </div>
+      <ul className="space-y-3 text-xl w-9/12">
+        <li className="flex justify-between border-l-4 pl-3 border-blue-500 bg-white rounded shadow p-3">
+            <p className='outline-none' >todo</p>
+            <p>status</p>
+        </li>
+        <li className="flex justify-between space-x-5 border-l-4 pl-3 border-blue-500 bg-white rounded shadow p-3">
+            <p className='outline-none' >todo</p>
+            <p>status</p>
+        </li>
+        <li className="flex justify-between space-x-5 border-l-4 pl-3 border-blue-500 bg-white rounded shadow p-3">
+            <p className='outline-none' >todo</p>
+            <p>status</p>
+        </li>
+      </ul>
+      <div>
+      <Link href="/">
+          <button
+            type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none"
+          >
+            フィルター
+          </button>
+        </Link>
+        <Link href="/">
+          <button
+            type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none"
+          >
+            ソート
+          </button>
+        </Link>
+        <Link href="/">
+          <button
+            type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none"
+          >
+            Todo作成
+          </button>
+        </Link>
+
+      </div>
+    </div>
+  )
+}
+
+export default page
