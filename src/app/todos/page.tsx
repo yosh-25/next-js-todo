69f487645f292c404b7128363ef2763055a4a857
yosh-25 <yoshi.okada25@gmail.com>
2024-02-15 23:40:04 +0900

    changed a little bit

--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -43,7 +43,7 @@ const page = () => {
             type="button"
             className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none"
           >
-            Todo作成
+            Todo新規作成
           </button>
         </Link>
 
