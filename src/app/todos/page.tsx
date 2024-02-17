9bcff7032e13f78804d7fbc27c083075ecb7ff45
yosh-25 <yoshi.okada25@gmail.com>
2024-02-17 21:05:35 +0900

    todo一覧のデザインを整えました

--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const page = () => {
   return (
@@ -7,22 +7,27 @@ const page = () => {
       <div>
         <h1 className="text-3xl font-bold">Todo一覧</h1>
       </div>
+
       <ul className="space-y-3 text-xl w-9/12">
-        <li className="flex justify-between border-l-4 pl-3 border-blue-500 bg-white rounded shadow p-3">
-            <p className='outline-none' >todo</p>
-            <p>status</p>
+        <li className="flex justify-between  pl-14 pr-14 ">
+          <p className="outline-none">タイトル</p>
+          <p>ステータス</p>
         </li>
-        <li className="flex justify-between space-x-5 border-l-4 pl-3 border-blue-500 bg-white rounded shadow p-3">
-            <p className='outline-none' >todo</p>
-            <p>status</p>
+        <li className="flex justify-between border-l-4 pl-14 pr-14 border-blue-500 bg-white rounded shadow p-3">
+          <p className="outline-none">todo</p>
+          <p>status</p>
         </li>
-        <li className="flex justify-between space-x-5 border-l-4 pl-3 border-blue-500 bg-white rounded shadow p-3">
-            <p className='outline-none' >todo</p>
-            <p>status</p>
+        <li className="flex justify-between space-x-5 border-l-4 pl-14 pr-14 border-blue-500 bg-white rounded shadow p-3">
+          <p className="outline-none">todo</p>
+          <p>status</p>
+        </li>
+        <li className="flex justify-between space-x-5 border-l-4 pl-14 pr-14 border-blue-500 bg-white rounded shadow p-3">
+          <p className="outline-none">todo</p>
+          <p>status</p>
         </li>
       </ul>
       <div>
-      <Link href="/">
+        <Link href="/">
           <button
             type="button"
             className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none"
@@ -46,10 +51,9 @@ const page = () => {
             Todo新規作成
           </button>
         </Link>
-
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
